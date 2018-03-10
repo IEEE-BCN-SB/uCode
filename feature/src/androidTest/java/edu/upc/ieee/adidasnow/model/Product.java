950de8302ded46351c30507d510ca68757e8a99f
johanitus1 <joan.vinyals@est.fib.upc.edu>
2018-03-10 10:58:00 +0100

    Model created

---
+++ feature/src/androidTest/java/edu/upc/ieee/adidasnow/model/Product.java
@@ -0,0 +1,53 @@
+package edu.upc.ieee.adidasnow.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Product {
+
+    @SerializedName("price")
+    @Expose
+    private Double price;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("category")
+    @Expose
+    private String category;
+    @SerializedName("description")
+    @Expose
+    private String description;
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
