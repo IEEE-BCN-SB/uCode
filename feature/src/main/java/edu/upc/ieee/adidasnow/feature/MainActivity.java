402e45dc18d6cc038221aa5e43babbc759aa2f70
melvintg <melvintg@users.noreply.github.com>
2018-03-10 15:48:02 +0100

    Menu upper bar added.

--- feature/src/main/java/edu/upc/ieee/adidasnow/feature/MainActivity.java
+++ feature/src/main/java/edu/upc/ieee/adidasnow/feature/MainActivity.java
@@ -1,10 +1,15 @@
 package edu.upc.ieee.adidasnow.feature;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ShareCompat;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -47,10 +52,51 @@ public class MainActivity extends AppCompatActivity {
         replaceFragment(BlankFragment.newInstance(getString(R.string.title_nav_home)));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_moreInfo) {
+            intentOpenWebPage("http://ieee.upc.edu/");
+        }
+        if (id == R.id.action_share) {
+            intentShareText("http://ieee.upc.edu/");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.container, fragment).commit();
+    }
+
+    public void intentOpenWebPage(String url) {
+        Uri uri = Uri.parse(url);
+
+        Intent internetIntent = new Intent(Intent.ACTION_VIEW, uri);
+
+        if (internetIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(internetIntent);
+        }
+
+    }
+
+    public void intentShareText(String text) {
+        String mimeType = "text/plain";
+        String title = "Share your item.";
+
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setChooserTitle(title)
+                .setType(mimeType)
+                .setText(text)
+                .startChooser();
     }
 
 }
